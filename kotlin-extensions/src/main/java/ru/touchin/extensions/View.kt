d1d35ceaa19ec83a0e328501c01df9315565b8ad
Denis Karmyshakov <denis.karmyshakov@gmail.com>
2018-10-20 13:54:39 +0300

    Simplification ripple click listener

--- kotlin-extensions/src/main/java/ru/touchin/extensions/View.kt
+++ kotlin-extensions/src/main/java/ru/touchin/extensions/View.kt
@@ -8,12 +8,11 @@ private const val RIPPLE_EFFECT_DELAY = 150L
 /**
  * Sets click listener to view. On click it will call something after delay.
  *
- * @param delay     Delay after which click listener will be called;
  * @param listener  Click listener.
  */
-fun View.setOnRippleClickListener(delay: Long = RIPPLE_EFFECT_DELAY, listener: () -> Unit) {
+fun View.setOnRippleClickListener(listener: () -> Unit) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        setOnClickListener { postDelayed({ if (hasWindowFocus()) listener() }, delay) }
+        setOnClickListener { postDelayed({ if (hasWindowFocus()) listener() }, RIPPLE_EFFECT_DELAY) }
     } else {
         setOnClickListener { listener() }
     }
