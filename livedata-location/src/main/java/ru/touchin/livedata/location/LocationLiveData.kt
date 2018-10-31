e29b1cdbee507be4873aebe8ad5727e2c1285368
Maxim Bachinsky <mbachinskiyol@gmail.com>
2018-10-31 15:00:58 +0300

    location livedata (#3)

---
+++ livedata-location/src/main/java/ru/touchin/livedata/location/LocationLiveData.kt
@@ -0,0 +1,61 @@
+package ru.touchin.livedata.location
+
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.content.Context
+import android.location.Location
+import androidx.annotation.RequiresPermission
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationAvailability
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+
+class LocationLiveData(
+        context: Context,
+        private val request: LocationRequest
+) : LiveData<Location>() {
+
+    private val fusedLocationClient: FusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context)
+    private val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult) {
+            postValue(result.lastLocation)
+        }
+
+        override fun onLocationAvailability(availability: LocationAvailability) {
+            if (!availability.isLocationAvailable) postValue(null)
+        }
+    }
+
+    @RequiresPermission(anyOf = [ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION])
+    override fun observe(owner: LifecycleOwner, observer: Observer<in Location>) {
+        super.observe(owner, observer)
+    }
+
+    @RequiresPermission(anyOf = [ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION])
+    override fun onActive() {
+        startListening()
+    }
+
+    override fun onInactive() {
+        stopListening()
+    }
+
+    private fun stopListening() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
+
+    @RequiresPermission(anyOf = [ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION])
+    private fun startListening() {
+        fusedLocationClient.requestLocationUpdates(
+                request,
+                locationCallback,
+                null
+        )
+    }
+
+}
