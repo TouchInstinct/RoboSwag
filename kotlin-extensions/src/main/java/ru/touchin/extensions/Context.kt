8f2e4271a679c3fd7d72e19d910be0e50616dab4
Oleg Kuznetsov <oleg.kuznetcov@touchin.ru>
2019-05-14 14:20:57 +0300

    added openBrowser and callToPhoneNumber methods

--- kotlin-extensions/src/main/java/ru/touchin/extensions/Context.kt
+++ kotlin-extensions/src/main/java/ru/touchin/extensions/Context.kt
@@ -2,7 +2,16 @@ package ru.touchin.extensions
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 
 fun Context.safeStartActivity(intent: Intent, options: Bundle? = null, resolveFlags: Int = 0): Boolean =
         packageManager.resolveActivity(intent, resolveFlags)?.let { startActivity(intent, options) } != null
+
+fun Context.openBrowser(url: String) = Intent(Intent.ACTION_VIEW)
+        .setData(Uri.parse(url))
+        .let { intent -> safeStartActivity(intent) }
+
+fun Context.callToPhoneNumber(phoneNumber: String) = Intent(Intent.ACTION_VIEW)
+        .setData(Uri.parse("tel:$phoneNumber"))
+        .let { intent -> safeStartActivity(intent) }
