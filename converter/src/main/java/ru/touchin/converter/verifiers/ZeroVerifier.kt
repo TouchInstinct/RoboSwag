cd8a6087aa615c1c4242cbbc036569c2bb54db2c
Artyom <artem-koma@yandex.ru>
2019-07-29 15:34:22 +0300

    Misc fixes for Zero and Point verifiers

--- converter/src/main/java/ru/touchin/converter/verifiers/ZeroVerifier.kt
+++ converter/src/main/java/ru/touchin/converter/verifiers/ZeroVerifier.kt
@@ -3,7 +3,7 @@ package ru.touchin.converter.verifiers
 import ru.touchin.converter.commands.Command
 import java.math.BigDecimal
 
-class ZeroVerifier(private val pointChar: Char = '.') : Verifier<BigDecimal> { // todo fix regular expression to catch repeating zeros
+class ZeroVerifier(val pointChar: Char) : Verifier<BigDecimal> { // todo fix regular expression to catch repeating zeros
 
     override fun verify(text: String): Command<BigDecimal> =
             if (text.length > 1 && text.startsWith('0') && !text.startsWith("0$pointChar")) {
