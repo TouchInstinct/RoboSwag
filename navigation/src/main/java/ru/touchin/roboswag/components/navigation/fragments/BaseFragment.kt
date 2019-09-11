8c5e101faa38aafdeaa235feef978a5695661c87
alex <pilotofsparrow@gmail.com>
2019-09-11 12:09:24 +0300

    Added fragment dependency v1.1.0 for Fragment constructor with arguments

--- navigation/src/main/java/ru/touchin/roboswag/components/navigation/fragments/BaseFragment.kt
+++ navigation/src/main/java/ru/touchin/roboswag/components/navigation/fragments/BaseFragment.kt
@@ -6,9 +6,7 @@ import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import androidx.annotation.ColorInt
 import androidx.annotation.ColorRes
 import androidx.annotation.DrawableRes
@@ -17,16 +15,14 @@ import androidx.annotation.LayoutRes
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.Lifecycle
 import butterknife.ButterKnife
 import ru.touchin.roboswag.components.navigation.BuildConfig
 import ru.touchin.roboswag.components.navigation.viewcontrollers.LifecycleLoggingObserver
 
-open class BaseFragment<TActivity : FragmentActivity, TState : Parcelable> constructor() : Fragment() {
+open class BaseFragment<TActivity : FragmentActivity, TState : Parcelable>(@LayoutRes layoutRes: Int) : Fragment(layoutRes) {
 
     companion object {
         private const val BASE_FRAGMENT_STATE_EXTRA = "BASE_FRAGMENT_STATE_EXTRA"
-        private const val BASE_FRAGMENT_LAYOUT_RES_EXTRA = "BASE_FRAGMENT_LAYOUT_RES_EXTRA"
 
         fun args(state: Parcelable?): Bundle = Bundle().also { it.putParcelable(BASE_FRAGMENT_STATE_EXTRA, state) }
 
@@ -46,41 +42,23 @@ open class BaseFragment<TActivity : FragmentActivity, TState : Parcelable> const
         }
     }
 
-    protected lateinit var view: View
-        @JvmName("requireView") get
-        private set
+    protected val view: View
+        @JvmName("requireViewKtx") get() = requireView()
 
-    protected lateinit var activity: TActivity
-        @JvmName("getTypedActivity") get
-        private set
+    protected val activity: TActivity
+        @JvmName("requireActivityKtx") get() = requireActivity() as TActivity
 
-    protected lateinit var context: Context
-        @JvmName("requireApplicationContext") get
-        private set
+    protected val context: Context
+        @JvmName("requireContextKtx") get() = requireContext()
 
     protected lateinit var state: TState
         private set
-
-    @LayoutRes
-    private var fragmentLayoutRes: Int? = null
-
-    constructor(@LayoutRes layoutRes: Int) : this() {
-        fragmentLayoutRes = layoutRes
-    }
-
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
-
-        activity = requireActivity() as TActivity
-        this.context = requireContext().applicationContext
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setHasOptionsMenu(true)
 
-        savedInstanceState?.getInt(BASE_FRAGMENT_LAYOUT_RES_EXTRA)?.takeIf { it != 0 }?.let { fragmentLayoutRes = it }
         state = savedInstanceState?.getParcelable<TState>(BASE_FRAGMENT_STATE_EXTRA)
                 ?: arguments?.getParcelable(BASE_FRAGMENT_STATE_EXTRA)
                         ?: throw IllegalStateException("Fragment state can't be null")
@@ -90,31 +68,16 @@ open class BaseFragment<TActivity : FragmentActivity, TState : Parcelable> const
         }
     }
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View {
-        view = inflater.inflate(
-                fragmentLayoutRes ?: throw IllegalStateException("Layout res can't be null"),
-                container,
-                false
-        )
-
-        ButterKnife.bind(this, view)
-
-        return view
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        ButterKnife.bind(this, view)
         lifecycle.addObserver(LifecycleLoggingObserver())
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putParcelable(BASE_FRAGMENT_STATE_EXTRA, state)
-        fragmentLayoutRes?.let { outState.putInt(BASE_FRAGMENT_LAYOUT_RES_EXTRA, it) }
     }
 
     fun <T : View> findViewById(@IdRes id: Int): T = view.findViewById(id)
