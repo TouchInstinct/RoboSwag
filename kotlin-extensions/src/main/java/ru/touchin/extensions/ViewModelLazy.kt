d6373ed69cce4902e79420c2271058ca391bb693
alex <pilotofsparrow@gmail.com>
2019-09-12 12:11:51 +0300

    Added lazy init methods for view models, with touch of roboswag flavor

---
+++ kotlin-extensions/src/main/java/ru/touchin/extensions/ViewModelLazy.kt
@@ -0,0 +1,34 @@
+package ru.touchin.extensions
+
+import androidx.activity.ComponentActivity
+import androidx.activity.viewModels
+import androidx.annotation.MainThread
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelStoreOwner
+import ru.touchin.lifecycle.viewmodel.LifecycleViewModelProviders
+
+@MainThread
+inline fun <reified VM : ViewModel> Fragment.viewModels(
+        noinline ownerProducer: () -> ViewModelStoreOwner = { this },
+        noinline factoryProducer: () -> ViewModelProvider.Factory = { LifecycleViewModelProviders.getViewModelFactory(this) }
+) = viewModels<VM>(ownerProducer, factoryProducer)
+
+@MainThread
+inline fun <reified VM : ViewModel> Fragment.targetViewModels(
+        noinline ownerProducer: () -> ViewModelStoreOwner = { targetFragment!! },
+        noinline factoryProducer: () -> ViewModelProvider.Factory = { LifecycleViewModelProviders.getViewModelFactory(targetFragment!!) }
+) = viewModels<VM>(ownerProducer, factoryProducer)
+
+@MainThread
+inline fun <reified VM : ViewModel> Fragment.activityViewModels(
+        noinline factoryProducer: () -> ViewModelProvider.Factory = { LifecycleViewModelProviders.getViewModelFactory(requireActivity()) }
+) = activityViewModels<VM>(factoryProducer)
+
+@MainThread
+inline fun <reified VM : ViewModel> ComponentActivity.viewModels(
+        noinline factoryProducer: () -> ViewModelProvider.Factory = { LifecycleViewModelProviders.getViewModelFactory(this) }
+): Lazy<VM> = viewModels(factoryProducer)
