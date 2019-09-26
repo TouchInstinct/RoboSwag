99ad9a85493848a2819da99991b8dedc8b07ade7
Ivan Vlasov <Blizzed@bk.ru>
2019-09-26 23:01:06 +0300

    Clear back stack before navigate to tab

--- tabbar-navigation/src/main/java/ru/touchin/roboswag/components/tabbarnavigation/BottomNavigationActivity.kt
+++ tabbar-navigation/src/main/java/ru/touchin/roboswag/components/tabbarnavigation/BottomNavigationActivity.kt
@@ -15,8 +15,21 @@ abstract class BottomNavigationActivity : NavigationActivity() {
     val innerNavigation: ViewControllerNavigation<BottomNavigationActivity>
         get() = getNavigationContainer(supportFragmentManager)?.navigation ?: navigation as ViewControllerNavigation<BottomNavigationActivity>
 
-    fun navigateTo(@IdRes navigationTabId: Int, state: Parcelable? = null) =
-            (supportFragmentManager.primaryNavigationFragment as? BottomNavigationFragment)?.navigateTo(navigationTabId, state)
+    /**
+     * Navigates to the given navigation tab.
+     * Can be called from any node of navigation graph so all back stack will be cleared.
+     *
+     * @param navigationTabId Id of navigation tab.
+     * @param state State of the given tab. If not null tab's fragment will be recreated, otherwise only in case it has not been created before.
+     */
+    fun navigateTo(@IdRes navigationTabId: Int, state: Parcelable? = null) {
+        supportFragmentManager.run {
+            // Clear all navigation stack unto the main bottom navigation (tagged as top)
+            popBackStackImmediate(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+
+            (primaryNavigationFragment as? BottomNavigationFragment)?.navigateTo(navigationTabId, state)
+        }
+    }
 
     private fun getNavigationContainer(fragmentManager: FragmentManager?): NavigationContainerFragment? =
             fragmentManager
