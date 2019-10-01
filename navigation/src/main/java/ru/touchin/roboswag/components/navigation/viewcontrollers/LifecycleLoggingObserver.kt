684c67fb85d326c0a38a192ff7292649d6789081
Nitser <trif.nitser@gmail.com>
2019-10-01 17:16:06 +0300

    rename call class

--- navigation/src/main/java/ru/touchin/roboswag/components/navigation/viewcontrollers/LifecycleLoggingObserver.kt
+++ navigation/src/main/java/ru/touchin/roboswag/components/navigation/viewcontrollers/LifecycleLoggingObserver.kt
@@ -7,37 +7,37 @@ import ru.touchin.roboswag.core.log.Lc
 import ru.touchin.roboswag.core.log.LcGroup
 
 class LifecycleLoggingObserver (
-        private val call: Any
+        private val call: lifecycleOwner
 ) : LifecycleObserver {
 
     @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
     fun onCreateLifecycleEvent() {
-        LcGroup.UI_LIFECYCLE.i(Lc.getCodePoint(call, "onCreate"))
+        LcGroup.UI_LIFECYCLE.i(Lc.getCodePoint(lifecycleOwner, "onCreate"))
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun onStartLifecycleEvent() {
-        LcGroup.UI_LIFECYCLE.i(Lc.getCodePoint(call, "onStop"))
+        LcGroup.UI_LIFECYCLE.i(Lc.getCodePoint(lifecycleOwner, "onStop"))
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
     fun onResumeLifecycleEvent() {
-        LcGroup.UI_LIFECYCLE.i(Lc.getCodePoint(call, "onResume"))
+        LcGroup.UI_LIFECYCLE.i(Lc.getCodePoint(lifecycleOwner, "onResume"))
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
     fun onPauseLifecycleEvent() {
-        LcGroup.UI_LIFECYCLE.i(Lc.getCodePoint(call, "onPause"))
+        LcGroup.UI_LIFECYCLE.i(Lc.getCodePoint(lifecycleOwner, "onPause"))
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun onStopLifecycleEvent() {
-        LcGroup.UI_LIFECYCLE.i(Lc.getCodePoint(call, "onStop"))
+        LcGroup.UI_LIFECYCLE.i(Lc.getCodePoint(lifecycleOwner, "onStop"))
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
     fun onDestroyLifecycleEvent() {
-        LcGroup.UI_LIFECYCLE.i(Lc.getCodePoint(call, "onDestroy"))
+        LcGroup.UI_LIFECYCLE.i(Lc.getCodePoint(lifecycleOwner, "onDestroy"))
     }
 
 }
