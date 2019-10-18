c3a1baa7c7a2c962764e81294bc105190da7f9c5
Oleg Kuznetsov <oleg.kuznetcov@touchin.ru>
2019-10-18 15:51:02 +0300

    small fix

--- navigation/src/main/java/ru/touchin/roboswag/components/navigation/keyboard_resizeable/KeyboardResizeableViewController.kt
+++ navigation/src/main/java/ru/touchin/roboswag/components/navigation/keyboard_resizeable/KeyboardResizeableViewController.kt
@@ -26,14 +26,14 @@ abstract class KeyboardResizeableViewController<TActivity : BaseActivity, TState
     private var isKeyboardVisible: Boolean = false
 
     private val keyboardHideListener = object : OnBackPressedListener {
-        override fun onBackPressed(): Boolean {
-            return if (isKeyboardVisible) {
-                UiUtils.OfViews.hideSoftInput(activity)
-                true
-            } else {
-                false
-            }
-        }
+
+        override fun onBackPressed(): Boolean =
+                if (isKeyboardVisible) {
+                    UiUtils.OfViews.hideSoftInput(activity)
+                    true
+                } else {
+                    false
+                }
     }
 
     private var isHideKeyboardOnBackEnabled = false
