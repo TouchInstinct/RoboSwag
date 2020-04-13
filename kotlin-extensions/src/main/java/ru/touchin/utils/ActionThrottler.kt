2df5e2e3c37ddd7da1f7010aa47526f9b152c474
Daniil Borisovskii <vender98@mail.ru>
2020-04-13 20:42:12 +0300

    Add explaining comment in `ActionThrottler`

--- kotlin-extensions/src/main/java/ru/touchin/utils/ActionThrottler.kt
+++ kotlin-extensions/src/main/java/ru/touchin/utils/ActionThrottler.kt
@@ -5,6 +5,8 @@ import ru.touchin.extensions.RIPPLE_EFFECT_DELAY
 
 object ActionThrottler {
 
+    // Multiplied by 2 because in interval after ripple effect finish and before
+    // action invoking start user may be in time to click and launch action again
     private const val DELAY = 2 * RIPPLE_EFFECT_DELAY
     private var lastActionTime = 0L
 
