f602f9ce13804b18c687361c6f7dbcba97918e8f
Daniil Shevtsov <me@daniilshevtsov.com>
2020-04-17 14:03:09 +0300

    lifecycle: add toLiveData extension for easy immutable live data fields creation

---
+++ lifecycle/src/main/java/ru/touchin/lifecycle/extensions/ImmutableExt.kt
@@ -0,0 +1,6 @@
+package ru.touchin.lifecycle.extensions
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+
+fun <T> MutableLiveData<T>.toLiveData() = this as LiveData<T>
