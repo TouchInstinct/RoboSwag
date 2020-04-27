175affafb3883c1935f46ffae3c0fa898e801cde
Daniil Borisovskii <vender98@mail.ru>
2020-04-27 13:11:04 +0300

    Added extension 'unwrapOrError' to main RX classes typed with Optional

--- rx-extensions/src/main/java/ru/touchin/extensions/rx/Observable.kt
+++ rx-extensions/src/main/java/ru/touchin/extensions/rx/Observable.kt
@@ -2,7 +2,18 @@ package ru.touchin.extensions.rx
 
 import io.reactivex.Completable
 import io.reactivex.Observable
+import ru.touchin.extensions.rx.utils.StringConstants
+import ru.touchin.roboswag.core.utils.Optional
+import ru.touchin.roboswag.core.utils.ShouldNotHappenException
 
 fun <T> Observable<T>.emitAfter(other: Completable): Observable<T> = this.flatMap { value ->
     other.andThen(Observable.just(value))
+}
+
+fun <T> Observable<Optional<T>>.unwrapOrError(
+        errorMessage: String = StringConstants.OPTIONAL_UNWRAPPING_ERROR_MESSAGE
+): Observable<T> = this.flatMap { wrapper ->
+    wrapper.get()
+            ?.let { Observable.just(it) }
+            ?: Observable.error(ShouldNotHappenException(errorMessage))
 }
