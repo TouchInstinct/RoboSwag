ba78cd441b10f990cb6b0a2550a3194c4746bc80
alex <pilotofsparrow@gmail.com>
2020-06-15 17:13:48 +0300

    added config.gradle to RoboSwag

---
+++ android-plugins/src/main/kotlin/plugins/lib-settings.gradle.kts
@@ -0,0 +1,27 @@
+
+plugins {
+    id("com.android.application") apply false
+    id("kotlin-android")
+    id("kotlin-android-extensions")
+}
+
+android {
+    compileSdkVersion(29)
+
+    defaultConfig {
+        minSdkVersion(21)
+    	targetSdkVersion(29)
+
+	    multiDexEnabled = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+
+}
