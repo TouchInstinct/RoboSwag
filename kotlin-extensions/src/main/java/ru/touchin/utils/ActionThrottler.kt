05bde29110d4aef5341fccf99ce00e8f942fcae9
Vladimir <vovaaksenov99@mail.ru>
2020-08-28 15:01:17 +0500

    Make univarsal throttling

--- kotlin-extensions/src/main/java/ru/touchin/utils/ActionThrottler.kt
+++ kotlin-extensions/src/main/java/ru/touchin/utils/ActionThrottler.kt
@@ -7,15 +7,15 @@ object ActionThrottler {
 
     // It is necessary because in interval after ripple effect finish and before
     // action invoking start user may be in time to click and launch action again
-    private const val PREVENTION_OF_CLICK_AGAIN_COEFFICIENT = 2
-    private const val DELAY_MS = PREVENTION_OF_CLICK_AGAIN_COEFFICIENT * RIPPLE_EFFECT_DELAY_MS
+    private const val PREVENTION_OF_REPEAT_COEFFICIENT = 2
+    private const val DEFAULT_DELAY_MS = PREVENTION_OF_REPEAT_COEFFICIENT * RIPPLE_EFFECT_DELAY_MS
     private var lastActionTime = 0L
 
-    fun throttleAction(action: () -> Unit): Boolean {
+    fun throttleAction(delay: Long = DEFAULT_DELAY_MS, action: () -> Unit): Boolean {
         val currentTime = SystemClock.elapsedRealtime()
         val diff = currentTime - lastActionTime
 
-        return if (diff >= DELAY_MS) {
+        return if (diff >= delay) {
             lastActionTime = currentTime
             action.invoke()
             true
