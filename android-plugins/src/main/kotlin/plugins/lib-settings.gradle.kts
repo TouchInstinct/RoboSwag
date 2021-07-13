d154ee198883225e7a4149a0dc510f63a95d991c
Alexander Korneev <alexander.korneev@touchin.ru>
2021-07-13 16:35:06 +0300

    Updating files for SDK30 on Licard

--- android-plugins/src/main/kotlin/plugins/lib-settings.gradle.kts
+++ android-plugins/src/main/kotlin/plugins/lib-settings.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    compileSdkVersion(29)
+    compileSdkVersion(30)
 
     defaultConfig {
         minSdkVersion(21)
-        targetSdkVersion(29)
+        targetSdkVersion(30)
     }
 
     compileOptions {
