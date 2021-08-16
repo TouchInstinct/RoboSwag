1a63c13680b29562d8b797152f80bdf5bf2ac92f
Rinat Nurmukhametov <rinat.nur.93@mail.ru>
2021-08-16 14:40:12 +0300

    changed the location of the parameters

--- mvi-arch/src/main/java/ru/touchin/roboswag/mvi_arch/core/IMvi.kt
+++ mvi-arch/src/main/java/ru/touchin/roboswag/mvi_arch/core/IMvi.kt
@@ -9,11 +9,8 @@ import ru.touchin.roboswag.mvi_arch.marker.ViewState
 /**
  * Interface with the main MVI methods and fields
  */
-interface IMvi<NavArgs, State, Action, VM>
-        where NavArgs : Parcelable,
-              State : ViewState,
-              Action : ViewAction,
-              VM : MviViewModel<NavArgs, Action, State> {
+interface IMvi<NavArgs : Parcelable, Action : ViewAction, State : ViewState, VM>
+        where VM : MviViewModel<NavArgs, Action, State> {
 
     /**
      * Use [viewModel] extension to get an instance of your view model class.
