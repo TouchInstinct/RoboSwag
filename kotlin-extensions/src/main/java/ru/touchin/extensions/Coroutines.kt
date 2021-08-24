e29bb344fdf4c6228785f89de33bc9e901c5a051
Kirill Nayduik <kirill.nayduikkn1@gmail.com>
2021-08-24 09:51:04 +0300

    Add context and start as params for safetyLaunch

--- kotlin-extensions/src/main/java/ru/touchin/extensions/Coroutines.kt
+++ kotlin-extensions/src/main/java/ru/touchin/extensions/Coroutines.kt
@@ -1,10 +1,17 @@
 package ru.touchin.extensions
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.CoroutineStart
 import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
+import kotlin.coroutines.EmptyCoroutineContext
 
-fun CoroutineScope.safetyLaunch(block: suspend () -> Unit) {
-    launch {
+fun CoroutineScope.safetyLaunch(
+        context: CoroutineContext = EmptyCoroutineContext,
+        start: CoroutineStart = CoroutineStart.DEFAULT,
+        block: suspend () -> Unit
+) {
+    launch(context, start) {
         try {
             block.invoke()
         } catch (_: Throwable) { }
