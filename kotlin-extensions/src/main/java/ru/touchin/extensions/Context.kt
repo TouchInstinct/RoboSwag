7b31ad84cc055b538c44ae61b0804eac1c0508e0
Vladimir Golyshev <3874873@gmail.com>
2021-09-13 02:31:27 +0300

    MOBILE-1144
    (cherry picked from commit d718d82e59e441386038de136305ba537a8a003b)

--- kotlin-extensions/src/main/java/ru/touchin/extensions/Context.kt
+++ kotlin-extensions/src/main/java/ru/touchin/extensions/Context.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.provider.Browser
 
 fun Context.safeStartActivity(intent: Intent, options: Bundle? = null, resolveFlags: Int = 0) =
         try {
@@ -14,6 +15,18 @@ fun Context.safeStartActivity(intent: Intent, options: Bundle? = null, resolveFl
 fun Context.openBrowser(url: String) = Intent(Intent.ACTION_VIEW)
         .setData(Uri.parse(url))
         .let { intent -> safeStartActivity(intent) }
+
+fun Context.openBrowserWithHeaders(url: String, headersMap: Map<String, String>) = Intent(Intent.ACTION_VIEW)
+        .setData(Uri.parse(url))
+        .let { intent ->
+            val bundle = Bundle().apply {
+                headersMap.forEach { (key, value) ->
+                    putString(key, value)
+                }
+            }
+            intent.putExtra(Browser.EXTRA_HEADERS, bundle)
+            safeStartActivity(intent)
+        }
 
 fun Context.callToPhoneNumber(phoneNumber: String) = Intent(Intent.ACTION_VIEW)
         .setData(Uri.parse("tel:$phoneNumber"))
