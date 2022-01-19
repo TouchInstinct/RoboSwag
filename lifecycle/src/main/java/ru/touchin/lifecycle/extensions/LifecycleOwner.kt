f58bdd92899306b31655ec85118de21447f3147e
Kirill Nayduik <kirill.nayduikkn1@gmail.com>
2022-01-19 13:14:47 +0300

    Fix detekt issues

--- lifecycle/src/main/java/ru/touchin/lifecycle/extensions/LifecycleOwner.kt
+++ lifecycle/src/main/java/ru/touchin/lifecycle/extensions/LifecycleOwner.kt
@@ -7,25 +7,17 @@ import kotlin.properties.ReadOnlyProperty
 
 fun <R : LifecycleOwner, T> R.onCreateEvent(
         initializer: (R) -> T
-): ReadOnlyProperty<R, T> {
-    return OnLifecycle(this, Lifecycle.Event.ON_CREATE, initializer)
-}
+): ReadOnlyProperty<R, T> = OnLifecycle(this, Lifecycle.Event.ON_CREATE, initializer)
 
 fun <R : LifecycleOwner, T> R.onStartEvent(
         initializer: (R) -> T
-): ReadOnlyProperty<R, T> {
-    return OnLifecycle(this, Lifecycle.Event.ON_START, initializer)
-}
+): ReadOnlyProperty<R, T> = OnLifecycle(this, Lifecycle.Event.ON_START, initializer)
 
 fun <R : LifecycleOwner, T> R.onResumeEvent(
         initializer: (R) -> T
-): ReadOnlyProperty<R, T> {
-    return OnLifecycle(this, Lifecycle.Event.ON_RESUME, initializer)
-}
+): ReadOnlyProperty<R, T> = OnLifecycle(this, Lifecycle.Event.ON_RESUME, initializer)
 
 fun <R : LifecycleOwner, T> R.onLifecycle(
         initializeEvent: Lifecycle.Event,
         initializer: (R) -> T
-): ReadOnlyProperty<R, T> {
-    return OnLifecycle(this, initializeEvent, initializer)
-}
+): ReadOnlyProperty<R, T> = OnLifecycle(this, initializeEvent, initializer)
