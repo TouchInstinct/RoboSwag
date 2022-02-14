f13984d7277409585af193702f2722d2616fb1a0
Kirill Nayduik <kirill.nayduikkn1@gmail.com>
2022-02-14 04:18:47 +0300

    Fix code style

--- lifecycle/src/main/java/ru/touchin/lifecycle/scope/ViewCoroutineScope.kt
+++ lifecycle/src/main/java/ru/touchin/lifecycle/scope/ViewCoroutineScope.kt
@@ -1,7 +1,10 @@
 package ru.touchin.lifecycle.scope
 
 import android.view.View
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancel
 import ru.touchin.lifecycle.R
 
 val View.viewScope: CoroutineScope
