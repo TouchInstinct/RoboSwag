331a8cc01bbfad1470dd39b8ff6b68639c9c3c51
Kirill Nayduik <kirill.nayduikkn1@gmail.com>
2022-04-18 19:07:18 +0300

    Make PendingRequestManager concurrent safety

--- network/src/main/java/ru/touchin/network/blocking/PendingRequestsManager.kt
+++ network/src/main/java/ru/touchin/network/blocking/PendingRequestsManager.kt
@@ -3,33 +3,41 @@ package ru.touchin.network.blocking
 import retrofit2.Call
 import retrofit2.Callback
 import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 object PendingRequestsManager {
 
     val isPending = AtomicBoolean(false)
 
+    private val pendingRequestsLock = ReentrantLock()
+
     private val pendingRequests = mutableListOf<Pair<Call<Any>, Callback<Any>>>()
 
+    fun getPendingRequestsCount() = pendingRequests.count()
+
     fun addPendingRequest(call: Call<Any>, callback: Callback<Any>) {
-        pendingRequests.add(call to callback)
+        pendingRequestsLock.withLock {
+            pendingRequests.add(call to callback)
+        }
     }
 
-    @Synchronized
     fun executePendingRequests() {
         applyActionToPendingRequests { first.enqueue(second) }
     }
 
-    @Synchronized
     fun dropPendingRequests() {
         applyActionToPendingRequests { first.cancel() }
     }
 
     private fun applyActionToPendingRequests(action: Pair<Call<Any>, Callback<Any>>.() -> Unit) {
-        isPending.set(false)
+        pendingRequestsLock.withLock {
+            isPending.set(false)
 
-        pendingRequests.forEach { it.action() }
+            pendingRequests.forEach { it.action() }
 
-        pendingRequests.clear()
+            pendingRequests.clear()
+        }
     }
 
 }
