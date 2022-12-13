a2622d11e447b32dd28af7192cdc90a9bf59ef68
airatmeister <zadrot.tat.99@gmail.com>
2022-12-13 15:42:22 +0300

    INTERNAL-376: "Replace" template generation from regex source expression

--- text-processing/src/main/java/ru/touchin/roboswag/textprocessing/TextProcessingBuilder.kt
+++ text-processing/src/main/java/ru/touchin/roboswag/textprocessing/TextProcessingBuilder.kt
@@ -9,6 +9,7 @@ import org.antlr.v4.runtime.tree.ParseTreeWalker
 
 class TextProcessingBuilder(regex: String, inputText: String) {
 
+    private var currentGroupIndex = 1
     private var _regexReplaceString = ""
     private var _formatString = ""
     private var _placeholder = ""
@@ -16,4 +17,35 @@ class TextProcessingBuilder(regex: String, inputText: String) {
     val regexReplaceString by ::_regexReplaceString
     val formatString by ::_formatString
     val placeholder by ::_placeholder
+
+    init {
+        regexToRegexReplace(regex)
+        format(inputText, regex)
+    }
+
+    private fun format(inputText: String, regex: String) {
+        _formatString = inputText.replace(Regex(regex), _regexReplaceString)
+    }
+
+    private fun regexToRegexReplace(regex: String) {
+        val stringStream = CharStreams.fromString(regex)
+        val lexer = PCRELexer(stringStream)
+        val parser = PCREParser(CommonTokenStream(lexer))
+        val parseContext = parser.parse()
+        val walker = ParseTreeWalker()
+        walker.walk(
+            object : PCREBaseListener() {
+                override fun enterCapture(ctx: PCREParser.CaptureContext) {
+                    super.enterCapture(ctx)
+                    _regexReplaceString += "\$$currentGroupIndex"
+                    currentGroupIndex++
+                }
+                override fun enterLiteral(ctx: PCREParser.LiteralContext) {
+                    super.enterLiteral(ctx)
+                    _regexReplaceString += ctx.shared_literal().text
+                }
+            },
+            parseContext
+        )
+    }
 }
